cd7a5d4dc3541fd28ff8ca7c8640ce469b778f44
arhamkih <123794035+arhamkih@users.noreply.github.com>
2023-02-06 22:55:11 +0700

    Create rombel.js

---
+++ rombel.js
@@ -0,0 +1,4 @@
+/* Digiarju | Rombel | */
+<script async='async' src='https://cdn.jsdelivr.net/gh/arhamkih/digishop/tektok.js'></script>
+<script async='async' src='https://cdn.jsdelivr.net/gh/arhamkih/digishop/bacalagi.js'></script>
+<script async='async' src='https://cdn.jsdelivr.net/gh/arhamkih/digishop/warna.js'></script>
