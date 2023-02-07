c3faf92e5754c1d4f0d46be818cf5bfa813f39bc
arhamkih <123794035+arhamkih@users.noreply.github.com>
2023-02-07 21:28:51 +0700

    Update rombel.js

--- rombel.js
+++ rombel.js
@@ -1,4 +1,14 @@
-/* Digiarju | Rombel | */
-<script async='async' src='https://cdn.jsdelivr.net/gh/arhamkih/digishop/tektok.js'></script>
-<script async='async' src='https://cdn.jsdelivr.net/gh/arhamkih/digishop/bacalagi.js'></script>
-<script async='async' src='https://cdn.jsdelivr.net/gh/arhamkih/digishop/warna.js'></script>
+var _0xf329 = ["value", "digjuTgl", "getElementById", "digjuBln", "digjuThn", "buatLagi", "href", "/p/nama-korea.html", "setAttribute", "digjukorea", "display", "style", "anon", "none", "digjukih", "block", "innerHTML", " ", "-"];
+function digiarjukorea() {
+ var devServer = document[_0xf329[2]](_0xf329[1])[_0xf329[0]];
+ var dupeNameCount = document[_0xf329[2]](_0xf329[3])[_0xf329[0]];
+ var groupNamePrefix = document[_0xf329[2]](_0xf329[4])[_0xf329[0]];
+ var _0xde7dx5 = document[_0xf329[2]](_0xf329[5]);
+ _0xde7dx5[_0xf329[8]](_0xf329[6], _0xf329[7]);
+ var cspDirectives = document[_0xf329[2]](_0xf329[9]);
+ document[_0xf329[2]](_0xf329[12])[_0xf329[11]][_0xf329[10]] = _0xf329[13];
+ document[_0xf329[2]](_0xf329[14])[_0xf329[11]][_0xf329[10]] = _0xf329[13];
+ document[_0xf329[2]](_0xf329[5])[_0xf329[11]][_0xf329[10]] = _0xf329[15];
+ cspDirectives[_0xf329[16]] += groupNamePrefix + _0xf329[17] + dupeNameCount + _0xf329[18] + devServer;
+}
+;
